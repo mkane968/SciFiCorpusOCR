09dfba33d7f07971332eceaba9a55c891b2d6700
Tim Bieniosek <bieniosek@gmail.com>
2019-09-24 14:26:31 -0400

    Fixed a bug or two
    A list of things fixed... be succinct but comprehensive

---
+++ tesseract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+tesseract SSFCBZ201710000327Y0000-ballarddrowned.tif SSFCBZ201710000327Y0000 --oem 1 -l eng tsv
+tesseract SSFCBZ201710000327Y0159-ballarddrowned.tif SSFCBZ201710000327Y0159 --oem 1 -l eng tsv
